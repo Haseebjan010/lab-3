13f05707209a80d3e703d2d85222395c8d81fa7e
Haseebjan010 <ha4051482@gmail.com>
2026-03-03 23:32:50 +0500

    Add files via upload

---
+++ lab task/LAB 2 TASK 3.cpp
@@ -0,0 +1,23 @@
+// Name: HAsseeb Riaz
+// Department: Electrical Engineering (Computing & Ai)
+// Registration No: BF25NWELE0697
+// Section: B
+
+
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+    	double F; // WORDS LIKHNY K LIYE
+    	float C; // DECIMAL VALUE K LIYE 
+    	cout<<"enter temperature in celcius ";  // DISPLAY
+    	cin>>C;
+    F = (C * 9/5) + 32;  // FORMULA CONVERRSION K LIYE
+    	cout<<F <<"  farheight";
+	
+
+return 0;
+}
